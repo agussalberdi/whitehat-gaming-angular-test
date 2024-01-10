629ef46549e25c71256bfce324555a5185c2bbe0
Agustin Alberdi <agustinalberdi1@gmail.com>
2024-01-10 11:42:39 -0300

    Unsubscribe for streams of data toa void memory leaks

--- src/app/modules/games/games.component.ts
+++ src/app/modules/games/games.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { map, takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs/Subject';
 import { JackpotManagerService } from '@shared/services/index';
 import { JackpotGame } from '@shared/interfaces/index';
 
@@ -10,7 +11,8 @@ import { JackpotGame } from '@shared/interfaces/index';
   templateUrl: './games.component.html',
   styleUrls: ['./games.component.scss']
 })
-export class GamesComponent implements OnInit {
+export class GamesComponent implements OnInit, OnDestroy {
+  destroySubject$: Subject<void> = new Subject();
   games$: Observable<JackpotGame[]>;
   currentCategories: string[] = [];
 
@@ -18,8 +20,8 @@ export class GamesComponent implements OnInit {
 
   ngOnInit(): void {
     this.currentCategories = this.route.snapshot.data.categories;
-    this.jackpotManagerService.getGamesWithJackpots().subscribe();
-    this.jackpotManagerService.updateGamesWithJackpots().subscribe();
+    this.jackpotManagerService.getGamesWithJackpots().pipe(takeUntil(this.destroySubject$)).subscribe();
+    this.jackpotManagerService.updateGamesWithJackpots().pipe(takeUntil(this.destroySubject$)).subscribe();
     this.fetchGames();
   }
 
@@ -27,6 +29,11 @@ export class GamesComponent implements OnInit {
     this.games$ = this.jackpotManagerService.gamesWithJackpots$.pipe(
       map((allGames: JackpotGame[]) => allGames.filter(game => game.categories.find(category => this.currentCategories.includes(category))))
     );
+  }
+
+
+  ngOnDestroy() {
+    this.destroySubject$.next();
   }
 
 }
